1cb252d943905e7109df9385add48f618724c3f4
Mahmoud19931010Elkot <mahmoudalihack.0000@gmail.com>
2025-06-22 17:24:49 +0100

    replace the file with the correct one

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("hi")
+import RPi.GPIO as GPIO
+import time
+
+# Use BCM numbering
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+LED_PIN = 11              # Pin 11 is GPIO 17
+
+# Setup pin
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(LED_PIN, GPIO.HIGH)  # Turn on
+        time.sleep(1)                    # Wait 1 second
+        GPIO.output(LED_PIN, GPIO.LOW)   # Turn off
+        time.sleep(1)                    # Wait 1 second
+except KeyboardInterrupt:
+    print("Stopped by user")
+finally:
+    GPIO.cleanup()
